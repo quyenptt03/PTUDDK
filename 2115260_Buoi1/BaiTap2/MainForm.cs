194f0b8c848dce23d8e8c0e9516368c6ad281ecb
Thao Quyen <112705874+quyenptt03@users.noreply.github.com>
2023-03-09 07:01:35 -0800

    Add files via upload

---
+++ 2115260_Buoi1/BaiTap2/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void rdNam_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdNam.Checked)
+                MessageBox.Show("Bạn đã chọn giới tính nam", "Thông báo");
+        }
+
+        private void rdNu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdNu.Checked)
+                MessageBox.Show("Bạn đã chọn giới tính nữ", "Thông báo");
+        }
+
+        private void btnToMau_Click(object sender, EventArgs e)
+        {
+            if (rdDo.Checked)
+                txtHopMau.BackColor = Color.Red;
+            if (rdXanh.Checked)
+                txtHopMau.BackColor = Color.Green;
+        }
+    }
+}
